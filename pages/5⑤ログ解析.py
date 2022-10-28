11c0e20460b9c16f6c92e32c8305e726ce6d9083
Ryusei0719 <103772720+320ryusei719@gmail.com>
2022-10-28 23:08:15 +0900

    1st

--- pages/5⑤ログ解析.py
+++ pages/5⑤ログ解析.py
@@ -17,7 +17,6 @@ with st.expander("使用した編成"):
     for name,weapon in st.session_state.Pweapon_dict.items():
         st.text(name+':'+weapon.get_text())
 
-st.session_state.simulate_log
 if 'simulate_log' not in st.session_state:
     st.text('先にシュミレーションをしてください')
     
