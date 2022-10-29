444f7aefdba1a44acc7911f2992973cd1a0795f5
Ryusei0719 <103772720+320ryusei719@gmail.com>
2022-10-29 17:17:58 +0900

    1st

--- TOP.py
+++ TOP.py
@@ -55,7 +55,7 @@ st.caption('これは編成と札回しを登録することでオデの勝率�
 
 st.write('左のスライドバーから順番に条件を設定することで、その条件での勝率や何ターンで締まるのかをシュミレーションすることができます。')
 
-st.subtitle('お問い合わせ')
+st.subheader('お問い合わせ')
 st.markdown('https://docs.google.com/forms/d/e/1FAIpQLSezsZWwtLLn3mfla98NMoXGE9t9E1aDlN1txtjPPEb8VjAm8g/viewform?usp=sf_link')
 
 #st.write(st.session_state)
