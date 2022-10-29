7831f25a2bc2bc2f2c73321d5afbb83eb381b2c8
Ryusei0719 <103772720+320ryusei719@gmail.com>
2022-10-29 17:37:13 +0900

    1st

--- func.py
+++ func.py
@@ -33,7 +33,6 @@ from google.oauth2 import service_account
 import gspread
 import pandas as pd
 # スプレッドシートの認証
-st.write(st.secrets)
 scopes = [ 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 credentials = service_account.Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=scopes)
 gc = gspread.authorize(credentials)
--- pages/4④シュミレーション.py
+++ pages/4④シュミレーション.py
@@ -10,29 +10,33 @@ critical_list =['p','p','p','p','p']
 weapon_list = [None,None,None,None,None]
 
 with st.form(key = 'sim_info'):
-    audition_name = st.selectbox(
-        'オーディション名',
-        audition_df.index.tolist(),
-        key = 'audition_name'
-    )    
+    cl1,cl2 = st.columns(2)
     
-    trend = st.selectbox(
-        '流行',
-        ['VoDaVi','VoViDa','DaVoVi','DaViVo','ViVoDa','ViDaVo']
-    )
-    
-    week = st.number_input(
-            label='経過週',
-            value = 29,
-            key = 'week'
+    with cl1:
+        audition_name = st.selectbox(
+            'オーディション名',
+            audition_df.index.tolist(),
+            key = 'audition_name'
+        )    
+        
+        trend = st.selectbox(
+            '流行',
+            ['VoDaVi','VoViDa','DaVoVi','DaViVo','ViVoDa','ViDaVo']
         )
-    memory_lv = st.number_input(
-        label = '思い出レベル',
-        step = 1,
-        min_value = 0,
-        max_value = 5,
-        key = 'memory_lv'
-    )
+        
+    with cl2:
+        week = st.number_input(
+                label='経過週',
+                value = 29,
+                key = 'week'
+            )
+        memory_lv = st.number_input(
+            label = '思い出レベル',
+            step = 1,
+            min_value = 0,
+            max_value = 5,
+            key = 'memory_lv'
+        )
     
     st.text('ステータス')
     status = {"Vo":None,"Da":None,"Vi":None,"Me":500,"name":"Myunit","P_idol":st.session_state.Pidol_name}
@@ -62,26 +66,28 @@ with st.form(key = 'sim_info'):
         )       
     
     st.markdown('***')
-    cols = st.columns(5)
+
     
-    for i,col in enumerate(cols):
-        with col:
-            st.text(f'{i+1}ターン目')
+    for i in range(5):
+        st.text(f'{i+1}ターン目')
+        cl1,cl2 = st.columns(2)
+        with cl1:
             aim_list[i] = st.selectbox(
                 '殴り先',
                 ('Vo','Da','Vi'),
                 key = f'aim{i}'
             )
+        with cl2:
             critical_list[i] = st.selectbox(
                 '判定',
                 ('Perfect','Good','Normal','Bad'),
                 key = f'critical{i}'
             )
-            weapon_list[i] = st.selectbox(
-                '札',
-                ['おまかせ','memory']+list(st.session_state.Pweapon_dict.keys())+st.session_state.support_list,
-                key = f'weapon{i}'
-            )
+        weapon_list[i] = st.selectbox(
+            '札',
+            ['おまかせ(殴り先に最も高いアピールをする札を切る)','おまかせ(全観客に与えるアピール値の総和が最も大きい札を切る)','memory']+list(st.session_state.Pweapon_dict.keys())+st.session_state.support_list,
+            key = f'weapon{i}'
+        )
             
     sim_btn = st.form_submit_button('シュミレーション開始')
     
@@ -89,7 +95,12 @@ with st.form(key = 'sim_info'):
         st.session_state.status = status
         st.session_state.trend = [trend[x:x+2] for x in range(0, len(trend), 2)]
         st.session_state.aim_list = aim_list
-        st.session_state.weapon_list = ['*' if x == 'おまかせ' else x for x in weapon_list]
+        for x in weapon_list:
+            if x == 'おまかせ(殴り先に最も高いアピールをする札を切る)':
+                x = '*'
+            elif x == 'おまかせ(全観客に与えるアピール値の総和が最も大きい札を切る):
+                x = '+'
+        st.session_state.weapon_list = weapon_list
         st.session_state.critical_list = [critical_full_name_dict[x] for x in critical_list]
         finish_flg = False
         with st.spinner('シュミレーション中'):
